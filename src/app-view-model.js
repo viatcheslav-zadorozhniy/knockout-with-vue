48ed973cf7669136457ad7b35f213adbc99c9e6d
ViatcheslavZadorozhniy <danashan0101@gmail.com>
2018-03-30 13:05:18 +0300

    Extend vue binding with autoBind and autoBindData parameters

--- src/app-view-model.js
+++ src/app-view-model.js
@@ -1,36 +1,38 @@
 define(['knockout', './vue-binding'], function (ko) {
-    return function AppViewModel() {
-        this.koFirstName = ko.observable('Viatcheslav');
-        this.koLastName = ko.observable('Zadorozhniy');
+    function AppViewModel() {
+        this.firstName = ko.observable('Viatcheslav');
+        this.lastName = ko.observable('Zadorozhniy');
 
         this.fullName = ko.pureComputed(function() {
-            return `Hello, ${this.koFirstName()} ${this.koLastName()}!`;
+            return `Hello, ${this.firstName()} ${this.lastName()}!`;
         }, this);
 
-        /*
-         * vueData - Data object which will be applied to Vue instance
-         * It should be a plain object or computed/pureComputed object
-         * If it is computed/pureComputed - it allows dynamically bind data from knockout to Vue
-         */
-        this.vueData = ko.pureComputed(function() {
-            return {
-                firstName: this.koFirstName(),
-                lastName: this.koLastName()
-            };
-        }, this);
+        // /*
+        //  * vueData - Data object which will be applied to Vue instance
+        //  * It should be a plain object or computed/pureComputed object
+        //  * If it is computed/pureComputed - it allows dynamically bind data from knockout to Vue
+        //  */
+        // this.vueData = ko.pureComputed(function() {
+        //     return {
+        //         firstName: this.firstName(),
+        //         lastName: this.lastName()
+        //     };
+        // }, this);
 
-        /*
-         * vueWatch - Plain object which contains map between Vue data (key) and knockout observables (value) which should be updated when related Vue property will be changed
-         * Alternatively you can pass callback function instead of knockout observables which will be called when related Vue property will be changed
-         */
-        this.vueWatch = {
-            firstName: this.koFirstName,
-            lastName: (newLastName) => {
-                if (newLastName !== this.koLastName()) {
-                    console.info('Last name was changed in Vue component!');
-                    this.koLastName(newLastName);
-                }
-            }
-        };
+        // /*
+        //  * vueWatch - Plain object which contains map between Vue data (key) and knockout observables (value) which should be updated when related Vue property will be changed
+        //  * Alternatively you can pass callback function instead of knockout observables which will be called when related Vue property will be changed
+        //  */
+        // this.vueWatch = {
+        //     firstName: this.firstName,
+        //     lastName: (newLastName) => {
+        //         if (newLastName !== this.lastName()) {
+        //             console.info('Last name was changed in Vue component!');
+        //             this.lastName(newLastName);
+        //         }
+        //     }
+        // };
     };
+
+    return AppViewModel;
 });
--- src/vue-binding.js
+++ src/vue-binding.js
@@ -11,48 +11,136 @@ define([
     'vue-components/todo-list-component'
 ], function (ko, Vue) {
     /*
-     * Example: <div data-bind="vue: { data: data, watch: watch }"></div>
+     * Examples:
+     * <div data-bind="vue: { autoBind: true }"><!-- Here you can place any Vue template markup --></div>
+     * <div data-bind="vue: { data: dataForVue, watch: observedVueProperties }"><!-- Here you can place any Vue template markup --></div>
 
-     * Parameter - "data":
-     * Data object which will be applied to Vue instance
-     * It should be a plain object or computed/pureComputed object
-     * If it is computed/pureComputed - it allows dynamically bind data from knockout to Vue
+     * Available parameters:
+     * data             (Type: Object | Computed | PureComputed)
+     * watch            (Type: { [key: string]: string | Function | Object | Array | Observable })
+     * methods          (Type: { [key: string]: Function })
+     * autoBind         (Type: boolean)
+     * autoBindData     (Type: boolean)
 
-     * Parameter - "watch":
-     * Plain object which contains map between Vue data (key) and knockout observables (value) which should be updated when related Vue property will be changed
-     * Alternatively you can pass callback function instead of knockout observables which will be called when related Vue property will be changed
+     * Parameter "data":
+     * Link: https://vuejs.org/v2/guide/instance.html#Data-and-Methods
+     * Details:
+     * Will be applied to Vue instance as data
+     * If it is Computed | PureComputed - Vue data will be automatically updated when the computed value will be changed
+
+     * Parameter "watch":
+     * Link: https://vuejs.org/v2/api/#watch
+     * Details:
+     * An object where keys are expressions to watch and values are corresponding callbacks.
+     * If it is { [key: string]: Observable } - when corresponding value from Vue instance will change, knockout Observable will be automatically updated
+
+     * Parameter "methods":
+     * Link: https://vuejs.org/v2/api/#methods
+     * Details:
+     * Methods to be mixed into Vue instance
+
+     * Parameter "autoBindData":
+     * Details:
+     * If it is "true" - all viewModel Observable will be setted as Vue instance data
+     * And Vue instance data will be updated automatically when corresponding Observable will change
+
+     * Parameter "autoBind":
+     * Details:
+     * If it is "true" - the same behaviour as for "autoBindData" will be applied
+     * Moreover all viewModel methods will be setted as "methods" for Vue instance
 
      */
     ko.bindingHandlers.vue = {
-        init: function (element, valueAccessor, allBindings) {
+        init: function (element, valueAccessor, allBindings, viewModel) {
             var params = ko.unwrap(valueAccessor());
-            var data = params.data;
-            var dataSubscription;
+            var subscriptions = [];
 
-            var vueInstance = new Vue({
-                el: element,
-                data: ko.unwrap(data),
-                watch: params.watch
+            var methods = params.methods || {};
+            var watch = params.watch || {};
+            var data = params.data || {};
+
+            if (params.autoBind || params.autoBindData) {
+                Object.keys(viewModel).forEach(function (key) {
+                    var value = viewModel[key];
+
+                    if (ko.isSubscribable(value)) {
+                        data[key] = ko.unwrap(value);
+
+                        subscriptions.push(
+                            value.subscribe(function (key, newValue) {
+                                if (data[key] !== newValue) {
+                                    data[key] = newValue;
+                                }
+                            }.bind(null, key))
+                        );
+
+                        if (params.autoBind && !ko.isComputed(value)) {
+                            watch[key] = value;
+                        }
+                    } else if (params.autoBind && typeof value === 'function') {
+                        methods[key] = value;
+                    }
+                });
+
+                if (params.autoBind && viewModel.constructor && viewModel.constructor.prototype) {
+                    Object.keys(viewModel.constructor.prototype).forEach(function (key) {
+                        var value = viewModel[key];
+
+                        if (typeof value === 'function' && !ko.isSubscribable(value)) {
+                            methods[key] = value;
+                        }
+                    });
+                }
+            }
+
+            // Bind "methods" to viewModel
+            Object.keys(methods).forEach(function (key) {
+                if (typeof methods[key] === 'function') {
+                    methods[key] = methods[key].bind(viewModel);
+                }
+            });
+
+            // Bind "watch" to viewModel
+            Object.keys(watch).forEach(function (key) {
+                if (typeof watch[key] === 'function') {
+                    watch[key] = watch[key].bind(viewModel);
+                }
             });
 
             if (ko.isSubscribable(data)) {
-                dataSubscription = data.subscribe(function (newData) {
-                    if (newData) {
-                        Object.keys(newData).forEach(function (key) {
-                            if (vueInstance.hasOwnProperty(key) && vueInstance[key] !== newData[key]) {
-                                vueInstance[key] = newData[key];
-                            }
-                        });
-                    }
-                });
+                subscriptions.push(
+                    data.subscribe(function (newValue) {
+                        if (vueInstance && newValue) {
+                            Object.keys(newValue).forEach(function (key) {
+                                if (vueInstance.hasOwnProperty(key) && vueInstance[key] !== newValue[key]) {
+                                    vueInstance[key] = newValue[key];
+                                }
+                            });
+                        }
+                    })
+                );
             }
 
-            ko.utils.domNodeDisposal.addDisposeCallback(element, function () {
-                vueInstance.$destroy();
+            // Create Vue instance
+            var vueInstance = new Vue({
+                el: element,
+                watch: watch,
+                methods: methods,
+                data: ko.unwrap(data)
+            });
 
-                if (dataSubscription) {
-                    dataSubscription.dispose();
-                }
+            /*
+             * "addDisposeCallback" should be added for "vueInstance.$el" but not for "element"
+             * Because Vue copy the element to which it is applied and remove original element from DOM
+             */
+            ko.utils.domNodeDisposal.addDisposeCallback(vueInstance.$el, function () {
+                // Destroy vueInstance and link to it
+                vueInstance.$destroy();
+                vueInstance = undefined;
+
+                // Clear all subscriptions
+                subscriptions.forEach(function (x) { x.dispose(); });
+                subscriptions = undefined;
             });
 
             return { controlsDescendantBindings: true };
